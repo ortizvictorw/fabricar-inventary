78ca034f1fc0c8ca71edf7efba92616d736bbf11
victor ortiz <ortiz.victor.w@gmail.com>
2025-02-12 15:28:27 -0300

    env

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
+    authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
+    projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
+    storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
+    appId: process.env['NG_APP_FIREBASE_APP_ID']
   },
   whitelist: ["fabricar.aberturas.daniel@gmail.com"]
 };
