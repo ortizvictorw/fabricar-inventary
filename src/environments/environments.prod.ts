8491e95e9b5d4debeea969e7e0b39a548d12387d
victor ortiz <ortiz.victor.w@gmail.com>
2025-02-12 15:04:26 -0300

    env

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
-    production: false,
-    firebaseConfig: {
-      apiKey: "",
-      authDomain: "",
-      projectId: "",
-      storageBucket: "",
-      messagingSenderId: "",
-      appId: ""
-    },
-    whitelist: []
-  };
-  
+  production: true,
+  firebaseConfig: {
+    apiKey: process.env.FIREBASE_API_KEY!,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_ID!,
+    appId: process.env.FIREBASE_APP_ID!
+  },
+  whitelist: process.env.WHITELIST?.split(',') ?? [] as string[] // ✅ Convierte el string en un array
+};
